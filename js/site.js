9cb765f9812cb3ba2d28550b459ff909d01029cd
Jan Monschke <jan.monschke@gmail.com>
2015-01-12 18:26:24 +0100

    move js to bottom of the page

--- js/site.js
+++ js/site.js
@@ -1,10 +1,8 @@
-window.onload = function(){
-  var navigationList = document.querySelector('.navigation-list');
+var navigationList = document.querySelector('.navigation-list');
 
-  var toggleMenu = function(){
-    console.log('toggle')
-    navigationList.classList.toggle('visible');
-  };
-
-  document.querySelector('.menu-opener').addEventListener('click', toggleMenu);
+var toggleMenu = function(){
+  console.log('toggle')
+  navigationList.classList.toggle('visible');
 };
+
+document.querySelector('.menu-opener').addEventListener('click', toggleMenu);
