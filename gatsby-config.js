391a55cafd61d154c69aa2828954b5474f81ff19
Jan Monschke <jan.monschke@gmail.com>
2019-06-12 09:05:04 +0200

    add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -107,6 +107,18 @@ module.exports = {
       options: {
         pathToConfigModule: `src/utils/typography`
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-19046336-4',
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true
+      }
     }
   ]
 };
