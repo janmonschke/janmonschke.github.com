556413dd855730da6d07dc009eaea88a6ab6c2cd
Jan Monschke <jan.monschke@gmail.com>
2019-09-27 19:12:33 +0200

    fix scrolling to hash links

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,17 @@ import 'typeface-source-serif-pro';
 
 // syntax highlighting theme
 require('prismjs/themes/prism.css');
+
+/* global window, document */
+const scrollTo = id => () => {
+  const el = document.querySelector(id);
+  if (el) return window.scrollTo(0, el.offsetTop - 20);
+  return false;
+};
+
+export const onRouteUpdate = ({ location: { hash } }) => {
+  console.log(location);
+  if (hash) {
+    window.setTimeout(scrollTo(hash), 10);
+  }
+};
