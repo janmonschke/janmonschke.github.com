5b003b0a04bed898cc806d438bee9f9d12f96942
Jan Monschke <jan.monschke@gmail.com>
2019-09-29 23:54:39 +0200

    finish v1 of landing page

--- src/pages/books/react-native-multiplayer-game.js
+++ src/pages/books/react-native-multiplayer-game.js
@@ -1,16 +1,104 @@
 import React from 'react';
-import cover from './assets/react-native-multiplayer-game-cover-v2.png';
-import Newsletter from '../../components/Newsletter';
+import cover from './assets/react-native-multiplayer-game-cover-v3.png';
 import './react-native-multiplayer-game.css';
 
 export default function ReactNativeMultiplayerGame() {
   return (
     <div className="reactNativeMGBook">
       <div className="reactNativeMGBook__bg"></div>
-      <h1>Learn how to build a multiplayer game</h1>
-      <h2>with ⚛️React Native and 🔥Firebase</h2>
-      <img src={cover} />
-      <Newsletter />
+      <div className="reactNativeMGBook__top">
+        <h1 className="reactNativeMGBook__headline">
+          Build a multiplayer game
+        </h1>
+        <h2 className="reactNativeMGBook__subHeadline">
+          with{' '}
+          <span aria-hidden role="presentation">
+            ⚛️
+          </span>{' '}
+          React Native and{' '}
+          <span aria-hidden role="presentation">
+            🔥
+          </span>{' '}
+          Firebase
+        </h2>
+      </div>
+      <p>
+        The book teaches the basics of <strong>React Native</strong> and{' '}
+        <strong>Firebase</strong> that you will need to build a fun mobile
+        multiplayer puzzle game. You will also learn how to build your own{' '}
+        <strong>server</strong> for your app.
+      </p>
+      <div className="reactNativeMGBook__coverAndFacts">
+        <img src={cover} className="reactNativeMGBook__cover" />
+        <div className="reactNativeMGBook__facts">
+          <p className="reactNativeMGBook__factsIntro ">
+            The book will teach you how to:
+          </p>
+          <ul className="reactNativeMGBook__factsList">
+            <li>
+              Build a game in <strong>React Native</strong>
+            </li>
+            <li>Add a multiplayer mode</li>
+            <li>
+              Model <strong>Firebase</strong> databases
+            </li>
+            <li>Build a multiplayer server</li>
+            <li>
+              Apply <strong>anti-cheating</strong> mechanisms
+            </li>
+            <li>Allow players to challenge their friends</li>
+            <li>
+              Port the game <strong>to the browser</strong>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <p>
+        Get notfied about updates on the book and get a{' '}
+        <strong>10% discount</strong>{' '}
+        <span aria-hidden role="presentation">
+          🎉
+        </span>{' '}
+        when the book is released by signing up for the newsletter:
+      </p>
+
+      <div>
+        <form
+          action="https://tinyletter.com/janmonschke"
+          method="post"
+          target="popupwindow"
+          onSubmit={openWindow}
+        >
+          <label
+            htmlFor="tlemail"
+            className="reactNativeMGBook__newsletterLabel"
+          >
+            Enter your email address:
+          </label>
+          <input
+            type="text"
+            name="email"
+            id="tlemail"
+            className="reactNativeMGBook__email"
+          />
+          <input type="hidden" value="1" name="embed" />
+          <input
+            type="submit"
+            value="Subscribe"
+            className="reactNativeMGBook__subscribe"
+          />
+        </form>
+      </div>
     </div>
   );
+}
+
+function openWindow() {
+  window.open(
+    'https://tinyletter.com/janmonschke',
+    'popupwindow',
+    'scrollbars=yes,width=800,height=600'
+  );
+  return true;
 }
