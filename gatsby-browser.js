0e322991ef7d6deff2a80fb953542c4100f67a42
Jan Monschke <janmonschke@fastmail.com>
2020-05-24 22:10:31 +0200

    don't log the location

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -13,7 +13,6 @@ const scrollTo = id => () => {
 };
 
 export const onRouteUpdate = ({ location: { hash } }) => {
-  console.log(location);
   if (hash) {
     window.setTimeout(scrollTo(hash), 10);
   }
