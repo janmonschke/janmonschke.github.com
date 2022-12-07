b01467207e21bf4adda4e87472fbb8b5c81cf54b
Jan Monschke <janmonschke@fastmail.com>
2022-12-07 15:59:26 +0100

    keep srcset and picture element

--- gatsby-config.js
+++ gatsby-config.js
@@ -174,12 +174,25 @@ function replaceBlurryImages(title, htmlWithImages) {
   const imageWrappers = $('.gatsby-resp-image-wrapper');
   console.log('replacing images', title, imageWrappers.length);
   imageWrappers.each((_, wrapper) => {
-    const actualImage = $('.gatsby-resp-image-image', wrapper);
-    actualImage.attr('src', `${url}${actualImage.attr('src')}`);
-    actualImage.attr('srcset', '');
-    actualImage.attr('sizes', '');
-    actualImage.attr('style', 'max-width: 100%');
-    $(wrapper).replaceWith(actualImage);
+    const picture = $('picture', wrapper);
+    const sources = $('source,img', picture);
+    sources.each((i, source) => {
+      const $sourceOrImg = $(source);
+      const srcset = $sourceOrImg.attr('srcset');
+      if (srcset) {
+        $sourceOrImg.attr(
+          'srcset',
+          srcset.replaceAll('/static', `${url}/static`)
+        );
+      }
+      const src = $sourceOrImg.attr('src');
+      if (src) {
+        $sourceOrImg.attr('src', src.replaceAll('/static', `${url}/static`));
+      }
+      $sourceOrImg.attr('sizes', '');
+      $sourceOrImg.attr('style', 'max-width: 100%');
+    });
+    $(wrapper).replaceWith(picture);
   });
 
   return $.html();
