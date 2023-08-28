41c4cb528640d73c2a722172f412544b258cb5b6
Jan Monschke <janmonschke@fastmail.com>
2023-08-28 10:01:46 +0200

    include videos

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,57 +1,56 @@
-const fs = require("fs");
-const path = require("path");
-const typography = require("./utils/typography");
-const { EleventyRenderPlugin } = require("@11ty/eleventy");
-const pluginRss = require("@11ty/eleventy-plugin-rss");
-const pluginWebc = require("@11ty/eleventy-plugin-webc");
-const Image = require("@11ty/eleventy-img");
-const markdownIt = require("markdown-it");
-const markdownItFootnote = require("markdown-it-footnote");
-const markdownItEleventyImg = require("markdown-it-eleventy-img");
-const renderBlogPicture = require("./utils/renderBlogPicture");
-const metadata = require("./_data/metadata");
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const { format, parseISO } = require("date-fns");
-const { CONTENT_DIR } = require("./utils/constants");
+const fs = require('fs');
+const path = require('path');
+const typography = require('./utils/typography');
+const { EleventyRenderPlugin } = require('@11ty/eleventy');
+const pluginRss = require('@11ty/eleventy-plugin-rss');
+const pluginWebc = require('@11ty/eleventy-plugin-webc');
+const Image = require('@11ty/eleventy-img');
+const markdownIt = require('markdown-it');
+const markdownItFootnote = require('markdown-it-footnote');
+const markdownItEleventyImg = require('markdown-it-eleventy-img');
+const renderBlogPicture = require('./utils/renderBlogPicture');
+const metadata = require('./_data/metadata');
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const { format, parseISO } = require('date-fns');
+const { CONTENT_DIR } = require('./utils/constants');
 const {
   getSlugFromPathName,
-  replaceUrlsAndAtMentions,
-} = require("./utils/webmentions");
+  replaceUrlsAndAtMentions
+} = require('./utils/webmentions');
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("assets");
-  eleventyConfig.addPassthroughCopy("content/**/*.kml");
-  eleventyConfig.addPassthroughCopy("CNAME");
+  eleventyConfig.addPassthroughCopy('assets');
+  eleventyConfig.addPassthroughCopy('CNAME');
 
-  eleventyConfig.addGlobalData("typographyStyles", typography.toString());
+  eleventyConfig.addGlobalData('typographyStyles', typography.toString());
 
   eleventyConfig.setFrontMatterParsingOptions({
     excerpt: true,
-    excerpt_separator: "<!-- more -->",
+    excerpt_separator: '<!-- more -->'
   });
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(EleventyRenderPlugin);
   eleventyConfig.addPlugin(pluginWebc, {
-    components: ["./_components/**/*.webc", "npm:@11ty/eleventy-img/*.webc"],
+    components: ['./_components/**/*.webc', 'npm:@11ty/eleventy-img/*.webc']
   });
 
   eleventyConfig.addPlugin(Image.eleventyImagePlugin, imageOptions);
 
   eleventyConfig.setLibrary(
-    "md",
+    'md',
     markdownIt({
       html: true,
       breaks: true,
-      linkify: true,
+      linkify: true
     })
       .use(markdownItFootnote)
       .use(markdownItEleventyImg, {
         imgOptions: {
-          ...imageOptions,
+          ...imageOptions
         },
         globalAttributes: {
-          ...imageOptions.defaultAttributes,
+          ...imageOptions.defaultAttributes
         },
         resolvePath: (filepath, env) => {
           return path.join(path.dirname(env.page.inputPath), filepath);
@@ -61,60 +60,60 @@ module.exports = function (eleventyConfig) {
             src,
             attributes,
             includeCaption: true,
-            linkToImage: true,
+            linkToImage: true
           });
-        },
+        }
       })
   );
 
-  eleventyConfig.addCollection("weeknotes", async function (collectionApi) {
+  eleventyConfig.addCollection('weeknotes', async function (collectionApi) {
     // Collections are sorted in ascending order in eleventy,
     // so we reversethat order for blog posts.
-    const blogPosts = [...collectionApi.getFilteredByTag("weeknote")].reverse();
-    console.log("weeknotes length", blogPosts.length);
+    const blogPosts = [...collectionApi.getFilteredByTag('weeknote')].reverse();
+    console.log('weeknotes length', blogPosts.length);
     return await Promise.all(mapPosts(blogPosts));
   });
 
-  eleventyConfig.addCollection("blogPosts", async function (collectionApi) {
+  eleventyConfig.addCollection('blogPosts', async function (collectionApi) {
     // Collections are sorted in ascending order in eleventy,
     // so we reversethat order for blog posts.
-    const blogPosts = [...collectionApi.getFilteredByTag("post")].reverse();
+    const blogPosts = [...collectionApi.getFilteredByTag('post')].reverse();
     return await Promise.all(mapPosts(blogPosts));
   });
 
   return {
     dir: {
       input: CONTENT_DIR,
-      includes: "../_includes",
-      data: "../_data",
-    },
+      includes: '../_includes',
+      data: '../_data'
+    }
   };
 };
 
 const imageOptions = {
-  formats: ["avif", "webp", "jpeg", "svg"],
+  formats: ['avif', 'webp', 'jpeg', 'svg'],
   svgShortCircuit: true,
 
-  urlPath: "/img",
+  urlPath: '/img',
 
   defaultAttributes: {
-    loading: "lazy",
-    decoding: "async",
+    loading: 'lazy',
+    decoding: 'async'
   },
 
   sharpWebpOptions: {
-    quality: 80,
+    quality: 80
   },
   sharpJpegOptions: {
-    quality: 90,
+    quality: 90
   },
   sharpAvifOptions: {
-    quality: 80,
-  },
+    quality: 80
+  }
 };
 
 function getFilename(fullPath) {
-  return fullPath.replace(/^.*[\\\/]/, "");
+  return fullPath.replace(/^.*[\\\/]/, '');
 }
 
 function mapPosts(posts) {
@@ -126,21 +125,21 @@ function mapPosts(posts) {
 
     blogPost.data.prevPost = prevPostObj && {
       title: prevPostObj.data.title,
-      url: prevPostObj.url,
+      url: prevPostObj.url
     };
 
     blogPost.data.nextPost = nextPostObj && {
       title: nextPostObj.data.title,
-      url: nextPostObj.url,
+      url: nextPostObj.url
     };
 
-    blogPost.data.dateDisplay = format(blogPost.data.date, "MMM dd, yyyy");
-    blogPost.data.dateDisplayLong = format(blogPost.data.date, "MMMM dd, yyyy");
+    blogPost.data.dateDisplay = format(blogPost.data.date, 'MMM dd, yyyy');
+    blogPost.data.dateDisplayLong = format(blogPost.data.date, 'MMMM dd, yyyy');
 
     const webmentionsFileName = `${getSlugFromPathName(blogPost.url)}.json`;
     const webmentionsFilePath = path.join(
-      "webmentions",
-      "data",
+      'webmentions',
+      'data',
       webmentionsFileName
     );
 
@@ -149,8 +148,8 @@ function mapPosts(posts) {
       const mentions = webmentions
         .filter(
           (wm) =>
-            wm["wm-property"] === "mention-of" ||
-            wm["wm-property"] === "in-reply-to"
+            wm['wm-property'] === 'mention-of' ||
+            wm['wm-property'] === 'in-reply-to'
         )
         .sort((a, b) => {
           // sort missing dates to the end
@@ -173,11 +172,11 @@ function mapPosts(posts) {
           mention.enrichedContent = replaceUrlsAndAtMentions(mention.content);
           mention.publishedDisplay = format(
             parseISO(mention.published),
-            "yyyy/MM/dd hh:mm"
+            'yyyy/MM/dd hh:mm'
           );
           return mention;
         });
-      const likes = webmentions.filter((wm) => wm["wm-property"] === "like-of");
+      const likes = webmentions.filter((wm) => wm['wm-property'] === 'like-of');
 
       blogPost.data.mentions = mentions;
       blogPost.data.likes = likes;
@@ -194,7 +193,7 @@ function mapPosts(posts) {
       let shareMetadata = await Image(coverImagePath, {
         ...imageOptions,
         widths: [600],
-        formats: ["jpeg"],
+        formats: ['jpeg']
       });
       blogPost.data.shareImageUrl =
         metadata.siteUrl + shareMetadata.jpeg[0].url;
